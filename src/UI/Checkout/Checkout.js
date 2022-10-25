de4df77f5d922ae5eccfc4f74dff0aa8af73b3e3
Raymon Shrestha <raymonshrestha@gmail.com>
2022-10-26 09:07:07 +1100

    E-commerce site
    Just the react boiler plate

---
+++ src/UI/Checkout/Checkout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Checkout.css'
+export default function () {
+  return (
+    <>
+    <div className='product'>
+        <div className="product-image">
+            <img src="/office.jpg" alt="office" />
+        </div>
+        <div className="product-details">
+            <h1>Product Name</h1> <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptas, quisquam voluptatibus enim sequi eligendi quia nihil rem nemo aperiam asperiores vel quae, officiis culpa cum neque eum? Sit, nulla.
+            </p>
+            <p>Price :</p>
+        </div>
+    </div>
+    </>
+  )
+}
