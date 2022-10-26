d25ded1b1889eb6d27e54f08755956bc9c38e702
Raymon Shrestha <raymonshrestha@gmail.com>
2022-10-26 15:23:24 +1100

    Used Ant design and added dropdown functionality
    Used ant design button and dropdown menu. Dropdown menus are clickable and the price can be changed

--- src/UI/Checkout/Checkout.js
+++ src/UI/Checkout/Checkout.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Checkout.css'
+import img from './office.jpg'
 export default function () {
   return (
     <>
-    <div className='product'>
+      <div className='product'>
         <div className="product-image">
-            <img src="/office.jpg" alt="office" />
+          <img src={img} alt="office" />
         </div>
         <div className="product-details">
-            <h1>Product Name</h1> <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptas, quisquam voluptatibus enim sequi eligendi quia nihil rem nemo aperiam asperiores vel quae, officiis culpa cum neque eum? Sit, nulla.
-            </p>
-            <p>Price :</p>
+          <h1>Product Name</h1> <br />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptas, quisquam voluptatibus enim sequi eligendi quia nihil rem nemo aperiam asperiores vel quae, officiis culpa cum neque eum? Sit, nulla.
+          </p>
+          <p>Price :</p>
         </div>
-    </div>
+      </div>
     </>
   )
 }
--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,17 +1,52 @@
 import React from 'react'
+import { useState } from 'react';
 import './Header.css'
+import { DownOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Menu, Space } from 'antd'
 export default function Header() {
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: <div className="label-1" onClick={handleClick}>AUD</div>,
+          key: '0'
+        },
+        {
+          label: <div className="label-2" onClick={handleClick}>IDR</div>,
+          key: '1'
+        },
+        {
+          label: <div className="label-3" onClick={handleClick}>CAD</div>,
+          key: '2'
+        },
+      ]}
+    />
+  );
+  const [price, setPrice] = useState('null')
+  function handleClick() {
+    var labels = document.querySelectorAll('div[class^=label]')
+    for (var i = 0; i < labels.length; i++) {
+      labels[i].addEventListener('click', function () {
+        setPrice(this.innerHTML)
+        console.log('click')
+      });
+    }
+  }
+
   return (
     <>
-        <nav className='nav-bar'>
-            <select name=" currency" id="currency">
-                <option value="AUD">AUD</option>
-                <option value="IDR">IDR</option>
-                <option value="CAD">CAD</option>
-            </select>
-            <button>Cart</button>
-            <button>Checkout</button>
-        </nav>
+      <nav className='nav-bar'>
+        <Dropdown overlay={menu}>
+          <Button>
+            Choose a Currency
+            <DownOutlined />
+          </Button>
+        </Dropdown>
+        <Button>Cart</Button>
+        <Button>Checkout</Button>
+      </nav>
+      <p>Price : {price}</p>
     </>
   )
 }
